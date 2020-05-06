ceefbb8c7acadf1e11e9a1f4b1a3a7d4a45998c2
Vojtěch Svoboda <46265429+vojtas131@users.noreply.github.com>
2020-05-06 22:57:22 +0200

    Tweeks

--- index.js
+++ index.js
@@ -19,14 +19,22 @@ function resolveAfter(time) {
         resolve('resolved');
       }, time);
     });
-  }
+}
 
-async function asyncCall(time, channel){
+async function updateResolve(channel, msg){
+    while(bet){
+        const result = await resolveAfter(600000);
+        client.say(channel, msg);
+    }
+}
+
+async function asyncCall(time, channel, msg, msgRepeat){
     console.log("Start");
+    updateResolve(channel, msgRepeat)
     const result = await resolveAfter(time);
     console.log("End")
     bet = false;
-    client.say(channel, "Betting ended, stay until the end of the stream to see who wins!");
+    client.say(channel, msg);
 }
 
 // TODO: 
@@ -35,21 +43,29 @@ async function asyncCall(time, channel){
 
 client.on('chat',(channel,userstate,message,self)=>{
     if(self)    return;
-    /*
     if(userstate.username=="vojtas131"){
         userstate.mod=true;
     }
-    */
 
     if(message=="!leaderboard"){
         rawdata = fs.readFileSync('leaderboard.json');
         result = JSON.parse(rawdata);
+
+        var items = Object.keys(result).map(function(key) {
+            return [key, result[key]];
+        });
+  
         client.say(channel, "Leaderboard:");
-        i = 1;
-        for (var key in result){
-            client.say(channel, i.toString() + ". " + key + " - " + result[key].toString());
-            i++;
-        }
+        i = 0;
+        var value = Infinity;
+        items = items.slice(0,5);
+        items.forEach((key) => {
+            if (value != key[1]){
+                i++;
+            }
+            client.say(channel, i.toString() + ". " + key[0] + " - " + key[1]);
+            value = key[1];
+        });
         return;
     }
 
@@ -62,12 +78,14 @@ client.on('chat',(channel,userstate,message,self)=>{
                 let messageString = message.split(" ");
                 let time = messageString[1];
                 (messageString[2].split(",")).forEach(i => {
-                    names.push(i);
+                    names.push(i.toLowerCase());
                 });
                 dict = {};
-                client.say(channel, "Runners: " + messageString[2] + " bet with !bet <runner> ex. !bet TheJazz");
+                console.log(names);
+                var msgRepeat = "Runners: " + messageString[2] + " bet with !bet <runner> ex. !bet TheJazz"
+                client.say(channel, msgRepeat);
                 console.log(parseInt(time)*60*1000);
-                return asyncCall((parseInt(time)*60*1000),channel);
+                return asyncCall((parseInt(time)*60*1000),channel,"Betting ended, stay until the end of the stream to see who wins!",msgRepeat);
             }
         }
         return;
@@ -111,6 +129,19 @@ client.on('chat',(channel,userstate,message,self)=>{
         client.say(channel, out);
         rawdata = fs.readFileSync('leaderboard.json');
         result = JSON.parse(rawdata);
+        var items = Object.keys(result).map((key) => {
+            return [key, result[key]];
+        });
+  
+        items.sort((first, second) => {
+          return second[1] - first[1];
+        });
+
+        result = {};
+        items.forEach((item) => {
+            result[item[0]]=item[1];
+        });
+
         winners.forEach(element => {
             if (element in result)   result[element]++;
             else    result[element] = 1;
